fdff628331926ab6b3f8d6ea70951ca7a1f6cc62
Võ Nhật Hào <vonhathao641@gmail.com>
2025-06-20 11:24:45 +0700

    minor changes

--- product_management_app/lib/presentation/widgets/state/empty_widget.dart
+++ product_management_app/lib/presentation/widgets/state/empty_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../../core/constants/app_colors.dart';
 
 class EmptyWidget extends StatelessWidget {
   const EmptyWidget({Key? key}) : super(key: key);
